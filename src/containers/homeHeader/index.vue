63d705b2d9af279cf6f730940196b93743a48f02
Daris <pedropaulodaris@gmail.com>
2022-03-01 21:26:12 -0300

    Style: adding footer

---
+++ src/containers/homeHeader/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <img src="@/assets/black_logo.png" alt="">
+        <textContent textClass="clock" text="Aqui vem o relógio."/>
+        <textContent textClass="weather" text="Aqui vem o clima."/>
+    </div>
+</template>
+
+<script>
+import textContent from '@/components/textContent/index.vue'
+// import titleContent from '@/components/title/index.vue'
+export default {
+    components: {
+        textContent,
+        // titleContent
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    @import "./index.scss";
+</style>
